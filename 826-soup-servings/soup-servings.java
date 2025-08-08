6aedcf899160c986e7c68f03162f19e2dd057e37
Alok Ranjan <alokranjan1106@gmail.com>
2025-08-08 08:26:17 +0530

    Time: 0 ms (100.00%) | Memory: 41.2 MB (93.41%) - LeetSync

--- 826-soup-servings/soup-servings.java
+++ 826-soup-servings/soup-servings.java
@@ -1,44 +1,32 @@
 class Solution {
+    private double[][] memo;
     public double soupServings(int n) {
-
-        if (n > 4800){
-            return 1;
-        }
+        //for very large n, probability approaches 1
+        if (n > 4800) return 1.0;
 
         int scaledN = (n + 24) / 25;
-        double [][] memo = new double[scaledN+1][scaledN+1];
+        memo = new double[scaledN+1][scaledN+1];
         for(double[] row : memo){
             Arrays.fill(row, -1.0);
         }
 
-        return dp(scaledN, scaledN, memo);
+        return dp(scaledN, scaledN);
     }
 
-    private static double dp(int a, int b, double[][] memo){
-        if (a <= 0 && b <= 0){
-            return 0.5;
-        }
+    private double dp(int a, int b){
+        if (a <= 0 && b <= 0) return 0.5;
+        if (a <= 0) return 1.0;
+        if (b <= 0) return 0.0;
 
-        if (a <= 0){
-            return 1.0;
-        }
-
-        if (b <= 0){
-            return 0.0;
-        }
-
-        if(memo[a][b] != -1){
-            return memo[a][b];
-        }
-
-        double res = 0.25 * (
-            dp(a-4, b, memo) +
-            dp(a-3, b-1, memo) +
-            dp(a-2, b-2, memo) +
-            dp(a-1, b-3, memo)
+        if(memo[a][b] != -1) return memo[a][b];
+        
+        memo[a][b] = 0.25 * (
+            dp(a-4, b) +      //100,0 -> 4 parts A, 0 parts B -> division by 25
+            dp(a-3, b-1) +    //75,25
+            dp(a-2, b-2) +    //50,50
+            dp(a-1, b-3)      //25,75
         );
 
-        memo[a][b] = res;
-        return res;
+        return memo[a][b];
     }
 }
