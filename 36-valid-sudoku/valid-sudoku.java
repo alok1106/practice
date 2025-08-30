1f0aca32e8bee75f15f921e9b87d4489ef707e97
Alok Ranjan <alokranjan1106@gmail.com>
2025-08-30 22:52:37 +0530

    Time: 1 ms (99.87%) | Memory: 44.3 MB (78.74%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.java
+++ 36-valid-sudoku/valid-sudoku.java
@@ -1,24 +1,38 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
-        HashSet<String> seen = new HashSet<>();
+        boolean[][] rows = new boolean[9][9];
+        boolean[][] cols = new boolean[9][9];
+        boolean[][] boxes = new boolean[9][9];
 
-        for (int i = 0; i < 9; ++i) {
-            for (int j = 0; j < 9; ++j) {
-                char currentVal = board[i][j];
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                char cell = board[i][j];
 
-                if (currentVal != '.') {
-                    String rowKey = currentVal + " in row " + i;
-                    String colKey = currentVal + " in col " + j;
-
-                    String boxKey = currentVal + " in box " + i / 3 + "-" + j / 3;
-
-                    if (!seen.add(rowKey) || !seen.add(colKey) || !seen.add(boxKey)) {
-                        return false;
-                    }
+                if (cell == '.') {
+                    continue;
                 }
+
+                int digit = cell - '1';
+
+                if (rows[i][digit]) {
+                    return false;
+                }
+                rows[i][digit] = true;
+
+                if (cols[j][digit]) {
+                    return false;
+                }
+                cols[j][digit] = true;
+
+                int boxIndex = (i / 3) * 3 + (j / 3);
+
+                if (boxes[boxIndex][digit]) {
+                    return false;
+                }
+                boxes[boxIndex][digit] = true;
             }
         }
-        
+
         return true;
     }
 }
