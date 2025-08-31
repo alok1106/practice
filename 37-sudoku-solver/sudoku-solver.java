df725c1a4345a515569eae88af1f1bd898f347c0
Alok Ranjan <alokranjan1106@gmail.com>
2025-08-31 22:53:32 +0530

    Time: 104 ms (92.82%) | Memory: 41.3 MB (33.65%) - LeetSync

--- 37-sudoku-solver/sudoku-solver.java
+++ 37-sudoku-solver/sudoku-solver.java
@@ -1,42 +1,46 @@
 class Solution {
     public void solveSudoku(char[][] board) {
-        solve(board);
+        boolean[][] rows = new boolean[9][9];
+        boolean[][] cols = new boolean[9][9];
+        boolean[][] boxes = new boolean[9][9];
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (board[i][j] != '.') {
+                    int digit = board[i][j] - '1';
+                    int boxIndex = (i / 3) * 3 + (j / 3);
+                    rows[i][digit] = true;
+                    cols[j][digit] = true;
+                    boxes[boxIndex][digit] = true;
+                }
+            }
+        }
+        solve(board, rows, cols, boxes);
     }
 
-    private boolean solve(char[][] board) {
+    private boolean solve(char[][] board, boolean[][] rows, boolean[][] cols, boolean[][] boxes) {
         for (int i = 0; i < 9; i++) {
             for (int j = 0; j < 9; j++) {
                 if (board[i][j] == '.') {
                     for (char c = '1'; c <= '9'; c++) {
-                        if (isValid(board, i, j, c)) {
+                        int digit = c - '1';
+                        int boxIndex = (i / 3) * 3 + (j / 3);
+                        if (!rows[i][digit] && !cols[j][digit] && !boxes[boxIndex][digit]) {
                             board[i][j] = c;
-
-                            if (solve(board)) {
+                            rows[i][digit] = true;
+                            cols[j][digit] = true;
+                            boxes[boxIndex][digit] = true;
+                            if (solve(board, rows, cols, boxes)) {
                                 return true;
                             } else {
                                 board[i][j] = '.';
+                                rows[i][digit] = false;
+                                cols[j][digit] = false;
+                                boxes[boxIndex][digit] = false;
                             }
                         }
                     }
                     return false;
                 }
-            }
-        }
-        return true;
-    }
-
-    private boolean isValid(char[][] board, int row, int col, char c) {
-        for (int i = 0; i < 9; i++) {
-            if (board[row][i] == c) {
-                return false;
-            }
-            if (board[i][col] == c) {
-                return false;
-            }
-            int subGridRow = 3 * (row / 3) + i / 3;
-            int subGridCol = 3 * (col / 3) + i % 3;
-            if (board[subGridRow][subGridCol] == c) {
-                return false;
             }
         }
         return true;
