dd7c4f4201a6d3fa500ac64217d4852f4939eeca
Alok Ranjan <alokranjan1106@gmail.com>
2025-09-27 15:54:14 +0530

    Time: 8 ms (62.33%) | Memory: 41.6 MB (41.51%) - LeetSync

--- 830-largest-triangle-area/largest-triangle-area.java
+++ 830-largest-triangle-area/largest-triangle-area.java
@@ -4,12 +4,16 @@ class Solution {
         double maxArea = 0.0;
 
         for (int i = 0; i < n - 2; i++) {
+            int[] P = points[i];
             for (int j = i + 1; j < n - 1; j++) {
+                int[] Q = points[j];
                 for (int k = j + 1; k < n; k++) {
+                    int[] R = points[k];
                     
+                    // Shoelace formula for area of triangle (P, Q, R)
                     double area = 0.5 * Math.abs(
-                          points[i][0] * points[j][1] + points[j][0] * points[k][1] + points[k][0] * points[i][1]
-                        - points[i][1] * points[j][0] - points[j][1] * points[k][0] - points[k][1] * points[i][0]
+                          P[0] * Q[1] + Q[0] * R[1] + R[0] * P[1]
+                        - P[1] * Q[0] - Q[1] * R[0] - R[1] * P[0]
                     );
                     
                     if (area > maxArea) {
