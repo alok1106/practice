240f31db337a377ed8e057c1d7a5cac2d45b60db
Alok Ranjan <alokranjan1106@gmail.com>
2025-10-18 18:57:53 +0530

    Time: 19 ms (84.14%) | Memory: 57.9 MB (81.38%) - LeetSync

--- 3620-maximum-number-of-distinct-elements-after-operations/maximum-number-of-distinct-elements-after-operations.java
+++ 3620-maximum-number-of-distinct-elements-after-operations/maximum-number-of-distinct-elements-after-operations.java
@@ -1,32 +1,21 @@
 class Solution {
     public int maxDistinctElements(int[] nums, int k) {
-        int n = nums.length;
-        int[][] ranges = new int[n][2];
-        
-        for (int i = 0; i < n; i++) {
-            ranges[i][0] = nums[i] - k;
-            ranges[i][1] = nums[i] + k;
-        }
-        
-        Arrays.sort(ranges, (a, b) -> Integer.compare(a[1], b[1]));
-        
+        Arrays.sort(nums);
         int curr = Integer.MIN_VALUE;
-        int distinct = 0;
-        
-        for (int[] range : ranges) {
-            int left = range[0];
-            int right = range[1];
-            
+        int count = 0;
+
+        for (int num : nums) {
+            int left = num - k;
+            int right = num + k;
+
             if (curr < left) {
                 curr = left;
             }
-            
-            if (curr >= left && curr <= right) {
-                distinct++;
+            if (curr <= right) {
+                count++;
                 curr++;
             }
         }
-        
-        return distinct;
+        return count;
     }
 }
