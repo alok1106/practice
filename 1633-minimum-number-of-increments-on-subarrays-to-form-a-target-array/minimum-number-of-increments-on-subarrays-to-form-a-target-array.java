1f609c9c0fa05f1e0fd57ee6fe2f7bdc9011d500
Alok Ranjan <alokranjan1106@gmail.com>
2025-10-30 09:19:17 +0530

    Time: 3 ms (100.00%) | Memory: 56 MB (98.31%) - LeetSync

--- 1633-minimum-number-of-increments-on-subarrays-to-form-a-target-array/minimum-number-of-increments-on-subarrays-to-form-a-target-array.java
+++ 1633-minimum-number-of-increments-on-subarrays-to-form-a-target-array/minimum-number-of-increments-on-subarrays-to-form-a-target-array.java
@@ -2,9 +2,7 @@ class Solution {
     public int minNumberOperations(int[] target) {
         int ops = target[0];
         for (int i = 1; i < target.length; i++) {
-            if (target[i] > target[i - 1]) {
-                ops += target[i] - target[i - 1];
-            }
+            ops += Math.max(target[i] - target[i - 1], 0);
         }
         return ops;
     }
